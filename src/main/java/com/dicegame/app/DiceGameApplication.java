49a4c387c89001feb9f1a38bb05fa723b48106b0
Basavaraj K N <Basavaraj.K@Stackroute.in>
2021-03-21 14:21:32 +0530

    Making application and its test cases run

---
+++ src/main/java/com/dicegame/app/DiceGameApplication.java
@@ -0,0 +1,8 @@
+package com.dicegame.app;
+
+public class DiceGameApplication {
+
+	public static void main(String[] args) {
+		System.out.println("=========== Welcome to Multi player Dice Game ===========");
+	}
+}
